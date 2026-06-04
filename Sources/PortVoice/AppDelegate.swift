b4db03d8373dd85245da93541572cea569ca67e6
Ko Pinpin <contact@xiaopinpinmusic.com>
2026-06-05 06:01:12 +0700

    Keep app running after closing window

---
+++ Sources/PortVoice/AppDelegate.swift
@@ -0,0 +1,11 @@
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        false
+    }
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+    }
+}
--- Sources/PortVoice/PortVoiceApp.swift
+++ Sources/PortVoice/PortVoiceApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct PortVoiceApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var appState = AppState()
 
     var body: some Scene {
